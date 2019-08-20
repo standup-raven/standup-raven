5e69b2719e8e2b4eaa0b1aa7d61b9bf85102640c
Harshil Sharma <harshilsharma63@gmail.com>
2019-08-21 05:05:18 +0530

    #GH-102 fixed styling

--- server/controller/middleware/authenticate.go
+++ server/controller/middleware/authenticate.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Authenticate middleware verifies the request was made by a logged in Mattermost user.
-// this is checked by the presence of Mattermost-User-Id HTTP header. 
+// this is checked by the presence of Mattermost-User-Id HTTP header.
 func Authenticate(w http.ResponseWriter, r *http.Request) *model.AppError {
 	userID := r.Header.Get(config.HeaderMattermostUserId)
 
