39817ad49e93c2e27f97df59a35a52436c93ba03
Harshil Sharma <harshilsharma63@gmail.com>
2020-09-15 20:09:20 +0530

    Prep for 3.1.1

--- server/migration/3_1_0.go
+++ server/migration/3_1_0.go
@@ -6,3 +6,10 @@ func upgradeDatabaseToVersion3_1_0(fromVersion string) error {
 	}
 	return nil
 }
+
+func upgradeDatabaseToVersion3_1_1(fromVersion string) error {
+	if UpdateErr := updateSchemaVersion(version3_1_1); UpdateErr != nil {
+		return UpdateErr
+	}
+	return nil
+}
