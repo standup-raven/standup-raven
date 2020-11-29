9e56932c1a5511ed38270b1ca03e8344faaca17f
Harshil Sharma <harshilsharma63@gmail.com>
2020-11-29 16:15:16 +0530

    Added migration test

---
+++ server/migration/3_2_0_test.go
@@ -0,0 +1,36 @@
+package migration
+
+import (
+	"errors"
+	"testing"
+
+	"bou.ke/monkey"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUpgradeDatabaseToVersion3_2_0(t *testing.T) {
+	defer TearDown()
+
+	updateSchemaVersionCount := 0
+	monkey.Patch(updateSchemaVersion, func(version string) error {
+		updateSchemaVersionCount++
+		return nil
+	})
+
+	err := upgradeDatabaseToVersion3_2_0(version3_1_0)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, updateSchemaVersionCount)
+
+	err = upgradeDatabaseToVersion3_2_0(version3_1_1)
+	assert.Nil(t, err)
+	assert.Equal(t, 2, updateSchemaVersionCount)
+
+	monkey.Patch(updateSchemaVersion, func(version string) error {
+		updateSchemaVersionCount++
+		return errors.New("simulated error")
+	})
+
+	err = upgradeDatabaseToVersion3_2_0(version3_1_1)
+	assert. NotNil(t, err)
+	assert.Equal(t, 3, updateSchemaVersionCount)
+}
