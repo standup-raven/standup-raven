214707106b8558d2d98499e48dd063f2a002f8c9
Harshil Sharma <harshilsharma63@gmail.com>
2020-11-29 16:30:57 +0530

    Removed redudent error checks

--- server/migration/3_2_0.go
+++ server/migration/3_2_0.go
@@ -1,8 +1,5 @@
 package migration
 
 func upgradeDatabaseToVersion3_2_0(fromVersion string) error {
-	if UpdateErr := updateSchemaVersion(version3_2_0); UpdateErr != nil {
-		return UpdateErr
-	}
-	return nil
+	return updateSchemaVersion(version3_2_0)
 }
