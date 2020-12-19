be6151662fc82305b9b03b7c0baf1168df6253d5
Harshil Sharma <harshilsharma63@gmail.com>
2020-12-20 04:42:22 +0530

    #GH-239 Fixed a bug preventing non-system admins to not see slash commadn autocomplete data

--- server/command/help.go
+++ server/command/help.go
@@ -9,6 +9,7 @@ func commandHelp() *Config {
 		AutocompleteData: &model.AutocompleteData{
 			Trigger:  "help",
 			HelpText: "Display Standup Raven help text.", // TODO prepare this help text
+			RoleID: model.SYSTEM_USER_ROLE_ID,
 		},
 		Validate: validateCommandHelp,
 		Execute:  executeCommandHelp,
