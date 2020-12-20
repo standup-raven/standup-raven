a6c556d541c7a4c956fae9e2caa78cc74a9586a5
Harshil Sharma <harshilsharma63@gmail.com>
2020-12-20 10:18:02 +0530

    #GH-243 Bumped version for release

--- server/migration/3_2_0.go
+++ server/migration/3_2_0.go
@@ -3,3 +3,7 @@ package migration
 func upgradeDatabaseToVersion3_2_0(fromVersion string) error {
 	return updateSchemaVersion(version3_2_0)
 }
+
+func upgradeDatabaseToVersion3_2_1(fromVersion string) error {
+	return updateSchemaVersion(version3_2_1)
+}
